fe90453317e906d2cb9f0896b59e40c12d614d00
quinntttt <quinn@torsius.net>
2025-12-04 11:33:32 +0100

    divided main into methods

--- Dag3/Dag3Deel1/Program.cs
+++ Dag3/Dag3Deel1/Program.cs
@@ -2,31 +2,39 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static string[] InputJoltage()
         {
             string readFromFile = File.ReadAllText("/home/quinn/Documents/Visual Studio Code/AdventOfCode/Dag3/input.txt");
-            string[] inputJoltage = readFromFile.Trim().Split("\n");
+            return readFromFile.Trim().Split("\n");
+        }
 
-            int outputTotalJoltage = 0;
-            int stringLength = inputJoltage[0].Length - 1;
-
-            foreach (string inputString in inputJoltage)
+        static int OutputJoltage(string inputString, int stringLength)
+        {
+            char highestNumber = char.Parse(inputString.Max().ToString());
+            int numberLocation = inputString.IndexOf(highestNumber);
+            if (numberLocation < stringLength)
             {
-                char highestNumber = char.Parse(inputString.Max().ToString());
-                int numberLocation = inputString.IndexOf(highestNumber);
-                if (numberLocation < stringLength)
-                {
-                    char secondHighestNumber = char.Parse(inputString.Substring(numberLocation + 1).Max().ToString());
-                    int outputJoltage = Convert.ToInt32(string.Format("{0}{1}", highestNumber, secondHighestNumber));
-                    outputTotalJoltage += outputJoltage;
-                }
-                else
-                {
-                    char highestNumber2 = char.Parse(inputString[..stringLength].Max().ToString());
-                    char secondHighestNumber = char.Parse(inputString[numberLocation..].Max().ToString());
-                    int outputJoltage = Convert.ToInt32(string.Format("{0}{1}", highestNumber2, secondHighestNumber));
-                    outputTotalJoltage += outputJoltage;
-                }
+                char secondHighestNumber = char.Parse(inputString.Substring(numberLocation + 1).Max().ToString());
+                int outputJoltage = Convert.ToInt32(string.Concat(highestNumber, secondHighestNumber));
+                return outputJoltage;
+            }
+            else
+            {
+                char highestNumber2 = char.Parse(inputString[..stringLength].Max().ToString());
+                char secondHighestNumber = char.Parse(inputString[numberLocation..].Max().ToString());
+                int outputJoltage = Convert.ToInt32(string.Concat(highestNumber2, secondHighestNumber));
+                return outputJoltage;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int outputTotalJoltage = 0;
+            int stringLength = InputJoltage()[0].Length - 1;
+
+            foreach (string inputString in InputJoltage())
+            {
+                outputTotalJoltage += OutputJoltage(inputString, stringLength);
             }
             Console.WriteLine($"Totale max capaciteit: {outputTotalJoltage}");
         }
