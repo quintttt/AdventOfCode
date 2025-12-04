928160086703a6dd7d38333cf47146470461daed
quinntttt <quinn@torsius.net>
2025-12-04 09:23:25 +0100

    cleaned code up and corrected variable names

--- Dag3/Dag3Deel1/Program.cs
+++ Dag3/Dag3Deel1/Program.cs
@@ -4,43 +4,31 @@
     {
         static void Main(string[] args)
         {
-            string[] inputJoltageTest = { "987654321111111", "811111111111119", "234234234234278", "818181911112111" };
-
             string readFromFile = File.ReadAllText("/home/quinn/Documents/Visual Studio Code/AdventOfCode/Dag3/input.txt");
             string[] inputJoltage = readFromFile.Split("\n");
-            int totalOutput = 0;
-            /* Note to self, wat hier gebeurt is: we checken het grootste nummer in een string. hierna checken met de index de locatie van deze char. \
-            als deze aan het einde staat maken we de string een stukje korter en zoeken we in de nieuwe string opnieuw naar het grootste cijfer. 
-            hierna, of als de char niet aan het einde maken we de string korter zodat we alleen nog het stuk ná de grootste char hebben,
-            waarin we dan weer naar het grootste karakter dáárin kunnen zoeken.
-            als we deze beiden hebben kunnen we ze samenvoegen en optellen bij het totaal aantal jolt. */
-            for (int i = 0; i < 200; i++)
+
+            int outputTotalJoltage = 0;
+            int stringLength = inputJoltage[0].Length - 1;
+
+            for (int i = 0; i < inputJoltage.Length - 1; i++)
             {
-                char max = char.Parse(inputJoltage[i].Max().ToString());
-                Console.WriteLine(max);
-                int location = inputJoltage[i].IndexOf(max);
-                // Console.WriteLine(location);
-                if (location < 99)
+                char highestNumber = char.Parse(inputJoltage[i].Max().ToString());
+                int numberLocation = inputJoltage[i].IndexOf(highestNumber);
+                if (numberLocation < stringLength)
                 {
-                    Console.WriteLine(inputJoltage[i].Substring(location + 1));
-                    char max2 = char.Parse(inputJoltage[i].Substring(location + 1).Max().ToString());
-                    int amogus = Convert.ToInt32(string.Format("{0}{1}", max, max2));
-                    Console.WriteLine($"max capaciteit: {amogus}");
-                    totalOutput += amogus;
+                    char secondHighestNumber = char.Parse(inputJoltage[i].Substring(numberLocation + 1).Max().ToString());
+                    int outputJoltage = Convert.ToInt32(string.Format("{0}{1}", highestNumber, secondHighestNumber));
+                    outputTotalJoltage += outputJoltage;
                 }
                 else
                 {
-                    char max1 = char.Parse(inputJoltage[i][..99].Max().ToString());
-                    Console.WriteLine(max1);
-                    // Console.WriteLine(location2);
-                    Console.WriteLine(inputJoltage[i].Substring(location + 1));
-                    char max2 = char.Parse(inputJoltage[i].Substring(location).Max().ToString());
-                    int amogus = Convert.ToInt32(string.Format("{0}{1}", max1, max2));
-                    Console.WriteLine($"max capaciteit: {amogus}");
-                    totalOutput += amogus;
+                    char highestNumber2 = char.Parse(inputJoltage[i][..stringLength].Max().ToString());
+                    char secondHighestNumber = char.Parse(inputJoltage[i][numberLocation..].Max().ToString());
+                    int outputJoltage = Convert.ToInt32(string.Format("{0}{1}", highestNumber2, secondHighestNumber));
+                    outputTotalJoltage += outputJoltage;
                 }
             }
-            Console.WriteLine(totalOutput);
+            Console.WriteLine($"Totale max capaciteit: {outputTotalJoltage}");
         }
     }
 }
