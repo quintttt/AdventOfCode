cfef10cc4f8080949d42c9f5c3618e05bb3a96f6
quinntttt <quinn@torsius.net>
2025-12-06 19:20:33 +0100

    cleaned up code

--- Dag6/Dag6Deel1/Program.cs
+++ Dag6/Dag6Deel1/Program.cs
@@ -8,28 +8,19 @@ namespace Dag6
             string readFromFile = File.ReadAllText("/home/quinn/Documents/Visual Studio Code/AdventOfCode/Dag6/input.txt");
             string[] inputMathArray = readFromFile.Split("\n");
 
-
             ulong count = 0;
             ulong[] mathNumberArray0 = Array.ConvertAll(inputMathArray[0].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries), ulong.Parse);
             ulong[] mathNumberArray1 = Array.ConvertAll(inputMathArray[1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries), ulong.Parse);
             ulong[] mathNumberArray2 = Array.ConvertAll(inputMathArray[2].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries), ulong.Parse);
             ulong[] mathNumberArray3 = Array.ConvertAll(inputMathArray[3].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries), ulong.Parse);
-            char[] mathNumberArray4 = Array.ConvertAll(inputMathArray[4].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries), char.Parse);
+            char[] mathOperatorArray0 = Array.ConvertAll(inputMathArray[4].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries), char.Parse);
 
             for (int i = 0; i < mathNumberArray0.Length; i++)
             {
-                if (mathNumberArray4[i] == '+')
-                {
-                    Console.WriteLine(mathNumberArray0[i] + mathNumberArray1[i] + mathNumberArray2[i] + mathNumberArray3[i]);
-                    count += mathNumberArray0[i] + mathNumberArray1[i] + mathNumberArray2[i] + mathNumberArray3[i];
-                }
-                if (mathNumberArray4[i] == '*')
-                {
-                    Console.WriteLine(mathNumberArray0[i] * mathNumberArray1[i] * mathNumberArray2[i] * mathNumberArray3[i]);
-                    count += mathNumberArray0[i] * mathNumberArray1[i] * mathNumberArray2[i] * mathNumberArray3[i];
-                }
+                if (mathOperatorArray0[i] == '+') { count += mathNumberArray0[i] + mathNumberArray1[i] + mathNumberArray2[i] + mathNumberArray3[i]; }
+                else if (mathOperatorArray0[i] == '*') { count += mathNumberArray0[i] * mathNumberArray1[i] * mathNumberArray2[i] * mathNumberArray3[i]; }
             }
-            Console.WriteLine(count);
+            Console.WriteLine($"Som van de worksheet: {count}");
         }
     }
 }
