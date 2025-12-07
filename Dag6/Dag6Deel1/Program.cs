3dae16055a16afe1d5a3837091cfff0b6dfe88ab
quinntttt <quinn@torsius.net>
2025-12-07 17:04:20 +0100

    cleaned up code

--- Dag6/Dag6Deel1/Program.cs
+++ Dag6/Dag6Deel1/Program.cs
@@ -6,48 +6,45 @@
         static ulong ExpressionParser(string mathExpression)
         {
             ulong[] mathNumbers = Array.ConvertAll(mathExpression.Split('+', '*'), ulong.Parse);
-            ulong result = mathNumbers[0];
+            ulong mathResult = mathNumbers[0];
             for (int i = 1; i < mathNumbers.Length; i++)
             {
-                if (mathExpression.Contains('+'))
-                {
-                    result += mathNumbers[i];
-                }
-                if (mathExpression.Contains('*'))
-                {
-                    result *= mathNumbers[i];
-                }
+                if (mathExpression.Contains('+')) { mathResult += mathNumbers[i]; }
+                if (mathExpression.Contains('*')) { mathResult *= mathNumbers[i]; }
             }
-            return result;
+            return mathResult;
         }
+
         static void Main(string[] args)
         {
             string readFromFile = File.ReadAllText("/home/quinn/Documents/Visual Studio Code/AdventOfCode/Dag6/input.txt");
             string[] inputMathArray = readFromFile.Split("\n", StringSplitOptions.RemoveEmptyEntries);
-            int[] mathNumberArray = Array.ConvertAll(inputMathArray[0].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries), int.Parse);
+            int[] intMathNumberArray = Array.ConvertAll(inputMathArray[0].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries), int.Parse);
+
             int i = 0;
             int j = 0;
-            ulong count = 0;
+            ulong totalCount = 0;
             char mathOperator = ' ';
             string mathExpression;
-            foreach (int mathColumn in mathNumberArray)
+
+            foreach (int mathColumn in intMathNumberArray)
             {
                 j = 0;
                 mathExpression = "";
+                
                 Dictionary<int, ulong> mathNumbers = new Dictionary<int, ulong>();
                 foreach (string inputMathString in inputMathArray)
                 {
                     if (!inputMathString.Contains('+') && !inputMathString.Contains('*'))
                     {
-                        ulong[] mathNumberArray0 = Array.ConvertAll(inputMathArray[j].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries), ulong.Parse);
-                        mathNumbers.Add(j, mathNumberArray0[i]);
+                        ulong[] mathNumberArray = Array.ConvertAll(inputMathArray[j].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries), ulong.Parse);
+                        mathNumbers.Add(j, mathNumberArray[i]);
                         if (j < (inputMathArray.Length - 2)) { j++; }
                     }
                     else if (inputMathString.Contains('+') || inputMathString.Contains('*'))
                     {
                         char[] mathOperatorArray = Array.ConvertAll(inputMathString.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries), char.Parse);
                         mathOperator = mathOperatorArray[i];
-                        Console.WriteLine(mathOperator);
                     }
                 }
                 foreach (var mathNumber in mathNumbers)
@@ -55,11 +52,10 @@
                     mathExpression += mathNumber.Value;
                     if (mathNumber.Key < mathNumbers.Count - 1) { mathExpression += mathOperator; }
                 }
-                Console.WriteLine(mathExpression);
-                count += ExpressionParser(mathExpression);
+                totalCount += ExpressionParser(mathExpression);
                 i++;
             }
-            Console.WriteLine(count);
+            Console.WriteLine(totalCount);
         }
     }
 }
